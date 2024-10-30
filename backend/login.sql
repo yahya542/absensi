ff237096c76beddcd46923ffd2cf00165ee14649
MUHAMMAD YAHYA ABDULLAHISSALAM <muhyahyaas@gmail.com>
2024-10-30 10:56:47 +0700

    perubahan

--- backend/login.sql
+++ backend/login.sql
@@ -17,6 +17,14 @@ CREATE TABLE users (
     npm VARCHAR(20) NOT NULL, 
     password VARCHAR(100) NOT NULL
 );
-INSERT INTO users (username, npm, password ) VALUES ('DIMAS', '22222', "$2b$12$Deya2501Hash");
+INSERT INTO users (username, npm, password ) VALUES ('DIMAS', '22222', "$2b$12$dimas22222Hash");
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(50) NOT NULL, 
+    npm VARCHAR(20) NOT NULL, 
+    password VARCHAR(100) NOT NULL
+);
+INSERT INTO users (username, npm, password ) VALUES ('MAKDUM', '22222', "$2b$12$mkdm11111Hash");
 
 
